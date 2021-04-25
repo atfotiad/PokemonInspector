acc4a2b18b7a8471e3ceaaa563048d9e0a81e22d
atfotiad <thfotiad@gmail.com>
2021-04-25 18:04:43 +0300

    added Navigation Component into MainActivity.kt

--- app/src/main/java/com/atfotiad/pokemoninspector/MainActivity.kt
+++ app/src/main/java/com/atfotiad/pokemoninspector/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.atfotiad.pokemoninspector
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.atfotiad.pokemoninspector.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initialize()
     }
+
+
+    private fun initialize() {
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.navHostFragment.id) as NavHostFragment
+        navController = navHostFragment.navController
+
+        bottomNavigationView = binding.navView
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+
+    }
+
 }
